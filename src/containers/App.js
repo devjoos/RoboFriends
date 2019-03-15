26dab7a9f0433fcf57a63908c2ce21ef1b0cf74a
Sam Joos <samjoos@Sams-MacBook-Pro.local>
2019-03-15 11:04:18 -0600

    addition of Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,45 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll'
 import Searchbox from '../components/Searchbox'
 import './App.css' 
+import { setSearchField, requestRobots } from '../actions.js'
 
+const mapStateToProps = state => {
+	return { 
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error }
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return { onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
 
 class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-	robots: [],
-	searchfield: ''
-}
-}
 
 componentDidMount() {
-	fetch('https://jsonplaceholder.typicode.com/users')
-	.then(response => response.json())
-	.then(users => this.setState({robots: users}))
+	this.props.onRequestRobots()
 }
 
-onSearchChange = (event) => {
-	this.setState({searchfield: event.target.value});
-	
 
-}
 	
 	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-		return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		const { searchField } = this.props;
+		const filteredRobots = this.props.robots.filter(robot => {
+		return robot.name.toLowerCase().includes(searchField.toLowerCase())
 	})
-		if (this.state.robots.length === 0) {
+		if (this.props.isPending) {
 			return <h1>Loading</h1>
 		} else {
 		return (
 		<div className='tc'>
 		<h1 className='f1'>RoboFriends</h1>
-		<Searchbox searchChange={this.onSearchChange}/>
+		<Searchbox searchChange={this.props.onSearchChange}/>
 		<Scroll>
 		<CardList robots={filteredRobots}/>
 		</Scroll>
@@ -47,5 +50,5 @@ onSearchChange = (event) => {
 }
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
